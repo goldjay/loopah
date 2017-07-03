f58f75dcbe27874ad8ddc05dc319c59aab767086
goldjay <jay.steingold@gmail.com>
2017-07-02 19:45:29 -0700

    Changed record functionality to button on button

--- Loopah/Loopah/AudioButton.swift
+++ Loopah/Loopah/AudioButton.swift
@@ -16,48 +16,46 @@ class AudioButton: UIButton, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
     
     var recordingSession: AVAudioSession!
     var audioRecorder: AVAudioRecorder!
-    var audioPlayer: AVAudioPlayer!
     
     var audioEngine: AVAudioEngine!
-    var avNode: AVAudioPlayerNode!
+    var playerA: AVAudioPlayerNode!
     var file: AVAudioFile!
     
-
+    
     var audioFileName: URL? = nil
     var btnSelected: Bool = false
     
-    var distorsionVal: Float = 0
-        /*
-     
-        */
+    var distorsionVal: Float = 0 {
+        didSet {
+            playerA.pause()
+            distortionUnit.wetDryMix = distorsionVal
+            
+            startAudioAfterChange()
+        }
+    }
     
     var pitchVal: Float = 0 {
         didSet {
+            playerA.pause()
             pitchUnit.pitch = pitchVal
+            startAudioAfterChange()
         }
     }
     var reverbVal: Float = 0 {
         didSet {
-            print("YOU SET THE VAL!")
-            avNode.stop()
+            playerA.pause()
             reverbUnit.wetDryMix = reverbVal
-            print(reverbUnit.wetDryMix)
-            
-            //avNode.scheduleFile(file, at: nil, completionHandler: nil)
-            
-            avNode.scheduleBuffer(buffer, at: nil, options: AVAudioPlayerNodeBufferOptions.loops, completionHandler: nil)
-            // Start the audio engine
-            audioEngine.prepare()
-            do{
-            try audioEngine.start()
-            avNode.play()
-            }catch{
-                print("Couldn't play")
-            }
+            startAudioAfterChange()
         }
     }
-        
     
+    var playBackVal: Float = 1 {
+        didSet {
+            playerA.pause()
+            pitchUnit.rate = playBackVal
+            startAudioAfterChange()
+        }
+    }
     
     let pitchUnit = AVAudioUnitTimePitch()
     let reverbUnit = AVAudioUnitReverb()
@@ -65,13 +63,21 @@ class AudioButton: UIButton, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
     
     var buffer: AVAudioPCMBuffer!
     
-    
-    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
         
+        let recBtn = UIButton(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        recBtn.backgroundColor = UIColor.red
+        
+        recBtn.layer.cornerRadius = 0.5 * recBtn.bounds.size.width
+        recBtn.layer.borderWidth = 2.0
+        recBtn.clipsToBounds = true
+        recBtn.layer.masksToBounds = true
+        recBtn.layer.zPosition = 10
+        recBtn.addTarget(self, action: #selector(startRecording), for: .touchUpInside)
+        addSubview(recBtn)
+        
         let btn = UIButton(frame: CGRect(x: 100, y: 0, width: 50, height: 50))
-        btn.backgroundColor = UIColor.red
         btn.layer.zPosition = 10
         btn.addTarget(self, action: #selector(handleSelectButtonPress(sender:)), for: .touchUpInside)
         addSubview(btn)
@@ -80,9 +86,9 @@ class AudioButton: UIButton, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
         
         let buttonTapped: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
         
-        let pressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handlePress(sender:)))
+        //let pressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handlePress(sender:)))
         
-        self.addGestureRecognizer(pressGestureRecognizer)
+        //self.addGestureRecognizer(pressGestureRecognizer)
         self.addGestureRecognizer(buttonTapped)
         
         // Set up Audio Recording
@@ -111,39 +117,42 @@ class AudioButton: UIButton, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
         print("HIT THE MENU BUTTON")
         
         if (self.layer.borderWidth == 5){
-                self.layer.borderWidth = 0
-                btnSelected = false
+            self.layer.borderWidth = 0
+            btnSelected = false
         }else {
             self.layer.borderWidth = 5
             btnSelected = true
         }
-        
-        
-
-        
     }
     
     func startRecording() {
-        audioFileName = getDocumentsDirectory().appendingPathComponent("recording\(self.tag).m4a")
         
-        print(audioFileName)
-        
-        let settings = [
-            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-            AVSampleRateKey: 12000,
-            AVNumberOfChannelsKey: 1,
-            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
-        ]
-        
-        do {
-            print("NOW RECORDING")
-            audioRecorder = try AVAudioRecorder(url: audioFileName!, settings: settings)
-            audioRecorder.delegate = self
-            audioRecorder.record()
+        // Check if you are currently recording
+        if(audioRecorder != nil && audioRecorder.isRecording) {
+            finishRecording(success: true)
+        } else {
+            audioFileName = getDocumentsDirectory().appendingPathComponent("recording\(self.tag).m4a")
             
-            //recordButton.setTitle("Tap to Stop", for: .normal)
-        } catch {
-            finishRecording(success: false)
+            print(audioFileName)
+            
+            let settings = [
+                AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                AVSampleRateKey: 12000,
+                AVNumberOfChannelsKey: 1,
+                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            ]
+            
+            // Change the title of the button to blink
+            do {
+                print("NOW RECORDING")
+                audioRecorder = try AVAudioRecorder(url: audioFileName!, settings: settings)
+                audioRecorder.delegate = self
+                audioRecorder.record()
+                
+                //recordButton.setTitle("Tap to Stop", for: .normal)
+            } catch {
+                finishRecording(success: false)
+            }
         }
     }
     
@@ -157,7 +166,7 @@ class AudioButton: UIButton, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
         audioRecorder.stop()
         audioRecorder = nil
         
-        avNode = AVAudioPlayerNode()
+        playerA = AVAudioPlayerNode()
         audioEngine = AVAudioEngine()
         
         if success {
@@ -167,19 +176,6 @@ class AudioButton: UIButton, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
             do {
                 playAudioEngine()
                 
-                /*
-                playing = true
-                audioPlayer = try AVAudioPlayer(contentsOf: audioFileName!)
-                audioPlayer.delegate = self
-                audioPlayer.prepareToPlay()
-                
-                audioPlayer.numberOfLoops = -1
-                audioPlayer.play()
- 
-            */
-                
-            } catch {
-                print(error)
             }
             print("ENDED OF ")
             
@@ -196,43 +192,35 @@ class AudioButton: UIButton, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
             buffer = AVAudioPCMBuffer(pcmFormat: file.processingFormat, frameCapacity: AVAudioFrameCount(file.length))
             try file.read(into: buffer)
             
-            // This is a reverb with a cathedral preset. It's nice and ethereal
-            // You're also setting the wetDryMix which controls the mix between the effect and the
-            // original sound.
-            
+            // Add some reverb
             reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.cathedral)
             reverbUnit.wetDryMix = 0
             
-            // This is a distortion with a radio tower preset which works well for speech
-            // As distortion tends to be quite loud you're setting the wetDryMix to only 25
+            // Add some distorsion
+            distortionUnit.loadFactoryPreset(AVAudioUnitDistortionPreset.speechRadioTower)
+            distortionUnit.wetDryMix = 0
             
-            distortionUnit.loadFactoryPreset(AVAudioUnitDistortionPreset.speechCosmicInterference)
-            distortionUnit.wetDryMix = 50
-            
-            pitchUnit.pitch = 2400
-            pitchUnit.rate = 32
+            pitchUnit.pitch = 0
             
             // Attach the four nodes to the audio engine
-            audioEngine.attach(avNode)
+            audioEngine.attach(playerA)
             audioEngine.attach(reverbUnit)
             audioEngine.attach(distortionUnit)
             audioEngine.attach(pitchUnit)
             
-            // Connect playerA to the reverb
-            audioEngine.connect(avNode, to: reverbUnit, format: buffer.format)
-            // Connect the reverb to the mixer
-            audioEngine.connect(reverbUnit, to: audioEngine.mainMixerNode, format: buffer.format)
-            // Connect the distortion to the mixer
+            // Connection chain
+            audioEngine.connect(playerA, to: pitchUnit, format: buffer.format)
+            audioEngine.connect(pitchUnit, to: reverbUnit, format: buffer.format)
+            audioEngine.connect(reverbUnit, to: distortionUnit, format: buffer.format)
             audioEngine.connect(distortionUnit, to: audioEngine.mainMixerNode, format: buffer.format)
-            // Connect the pitch to the mixer
-            audioEngine.connect(pitchUnit, to: audioEngine.mainMixerNode, format: buffer.format)
             
             // Schedule sound to play on buffer in a loop
-            avNode.scheduleBuffer(buffer, at: nil, options: AVAudioPlayerNodeBufferOptions.loops, completionHandler: nil)
+            playerA.scheduleBuffer(buffer, at: nil, options: AVAudioPlayerNodeBufferOptions.loops, completionHandler: nil)
+            
             // Start the audio engine
             audioEngine.prepare()
             try audioEngine.start()
-            avNode.play()
+            playerA.play()
         } catch {
             print("Playing didn't work")
         }
@@ -249,49 +237,52 @@ class AudioButton: UIButton, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
     
     func handleTap(sender: UITapGestureRecognizer) {
         print("TAPPED!")
-        
-        if avNode.isPlaying {
-            avNode.pause()
-            
-        } else {
-            // If there is a file, play
-            if((audioFileName) != nil){
-            avNode.play()
-            //audioPlayer.numberOfLoops = -1
-            //audioPlayer.play()
-            
+        if(audioFileName != nil){
+            if playerA.isPlaying {
+                playerA.pause()
+            } else {
+                playerA.play()
             }
         }
     }
+    /*
+     
+     func handlePress(sender: UILongPressGestureRecognizer) {
+     if sender.state == UIGestureRecognizerState.began {
+     // handle start of pressing
+     print("Start press")
+     startRecording()
+     }
+     else if sender.state == UIGestureRecognizerState.ended {
+     // handle end of pressing
+     print("End press")
+     finishRecording(success: true)
+     }
+     }
+     */
+    /*
+     func handleDoubleTap(sender: UITapGestureRecognizer) {
+     print("DOUBLE TAP")
+     if sender.state == .ended {
+     print("UIGestureRecognizerStateEnded")
+     //Do Whatever You want on End of Gesture
+     }
+     else if sender.state == .began {
+     print("UIGestureRecognizerStateBegan.")
+     //Do Whatever You want on Began of Gesture
+     }
+     }
+     */
     
-    func handleLongPress(sender: UILongPressGestureRecognizer) {
-        print("LONG PRESS")
-        
-    }
-    
-    func handlePress(sender: UILongPressGestureRecognizer) {
-        if sender.state == UIGestureRecognizerState.began {
-            // handle start of pressing
-            print("Start press")
-            startRecording()
+    func startAudioAfterChange() {
+        playerA.scheduleBuffer(buffer, at: nil, options: AVAudioPlayerNodeBufferOptions.loops, completionHandler: nil)
+        // Start the audio engine
+        audioEngine.prepare()
+        do{
+            try audioEngine.start()
+            playerA.play()
+        }catch{
+            print("Couldn't play")
         }
-        else if sender.state == UIGestureRecognizerState.ended {
-            // handle end of pressing
-            print("End press")
-            finishRecording(success: true)
-        }
-    }
-    
-    func handleDoubleTap(sender: UITapGestureRecognizer) {
-        print("DOUBLE TAP")
-        if sender.state == .ended {
-            print("UIGestureRecognizerStateEnded")
-            //Do Whatever You want on End of Gesture
-        }
-        else if sender.state == .began {
-            print("UIGestureRecognizerStateBegan.")
-            //Do Whatever You want on Began of Gesture
-        }
-        
     }
 }
--- Loopah/Loopah/ViewController.swift
+++ Loopah/Loopah/ViewController.swift
@@ -9,50 +9,112 @@
 import UIKit
 import AVFoundation
 
+enum Shape {
+    case Triangle, Circle
+}
+
+class ShapeButton : UIButton
+{
+    var shapeColor:UIColor!
+    var buttonShape:Shape!
+}
+
 class ViewController: UIViewController {
     
     var mySound: AVAudioPlayer!
-    //var coder: NSDecoder
-
+    
+    @IBOutlet weak var playButton: ShapeButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        /*
+         let button1  = UIButton(type:  .custom)
+         button1.frame = CGRect(x:100, y:50, width:50, height:50)
+         if let image = UIImage(named:"circle.png") {
+         button1.setImage(image, for: .normal)
+         }
+         self.view.addSubview(button1)
+         */
+        
+        let button2  = playButton!
+        button2.shapeColor = UIColor.red
+        button2.buttonShape = Shape.Triangle
+        button2.frame = CGRect(x:190, y:30, width:30, height:30)
+        if let image = drawCustomImage(size:button2.frame.size,imageShape: button2.buttonShape,color:button2.shapeColor) {
+            button2.setImage(image, for: .normal)
+        }
+        button2.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
+        self.view.addSubview(button2)
+        
+        /*
+         let button3  = ShapeButton(type:  .custom)
+         button3.buttonShape = Shape.Circle
+         button3.shapeColor = UIColor.green
+         button3.frame = CGRect(x:190, y:30, width:30, height:30)
+         if let image = drawCustomImage(size:button3.frame.size,imageShape: button3.buttonShape,color:button3.shapeColor) {
+         button3.setImage(image, for: .normal)
+         }
+         self.view.addSubview(button3)
+         */
     }
-
+    
+    func drawCustomImage(size: CGSize,imageShape:Shape,color:UIColor) -> UIImage? {
+        // Setup our context
+        let bounds = CGRect(origin: CGPoint.zero, size: size)
+        let opaque = false
+        let scale: CGFloat = 0
+        UIGraphicsBeginImageContextWithOptions(size, opaque, scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        
+        // Setup complete, do drawing here
+        context.setStrokeColor(color.cgColor) //UIColor.red.cgColor
+        context.setLineWidth(1)
+        
+        // Would draw a border around the rectangle
+        // context.stroke(bounds)
+        
+        context.beginPath()
+        if imageShape == Shape.Triangle {
+            context.move(to: CGPoint(x: bounds.minX, y: bounds.maxY))
+            context.addLine(to: CGPoint(x: bounds.maxX, y: bounds.maxY))
+            context.addLine(to: CGPoint(x: bounds.maxX/2, y: bounds.minY))
+            context.addLine(to: CGPoint(x: bounds.minX, y: bounds.maxY))
+        } else {
+            context.addEllipse(in: bounds)
+        }
+        context.closePath()
+        
+        context.setFillColor(color.cgColor)
+        context.fillPath()
+        
+        // Drawing complete, retrieve the finished image and cleanup
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     @IBAction func playFile(_ sender: AnyObject) {
         
-        let path = Bundle.main.path(forResource: "recording1.m4a", ofType:nil)!
-        let url = URL(fileURLWithPath: path)
+        //let path = Bundle.main.path(forResource: "recording1.m4a", ofType:nil)!
+        //let url = URL(fileURLWithPath: path)
         
-        do {
-            //let mySound = try AVAudioPlayer(contentsOf: url)
-            //sound = sound
-            //sound.play()
-        } catch {
-            print("Couldn't load file :(")
-        }
+        
     }
-
-    @IBAction func sliderChanged(_ sender: UISlider) {
-        print(sender.value)
-        
+    @IBAction func pitchChanged(_ sender: UISlider) {
         sender.isContinuous = false // stops continuous updating
         
         for case let btn as AudioButton in self.view.subviews {
-            print(btn.btnSelected)
-            if(btn.isSelected){
-                btn.avNode.pause()
-                btn.avNode.scheduleBuffer(btn.buffer, at: nil, options: AVAudioPlayerNodeBufferOptions.loops, completionHandler: nil)
+            if(btn.btnSelected){
                 btn.pitchVal = sender.value
-                btn.avNode.play()
-                print("PitchVal is now \(btn.pitchVal)")
             }
         }
     }
+    
     @IBAction func reverbChanged(_ sender: UISlider) {
         sender.isContinuous = false // stops continuous updating
         print(sender.value)
@@ -60,78 +122,46 @@ class ViewController: UIViewController {
         for case let btn as AudioButton in self.view.subviews {
             if(btn.btnSelected){
                 btn.reverbVal = sender.value
-                print("SELECTED")
-                print(btn.reverbVal)
-                /*
-                switch(sender.value){
-                    
-                    
-                case 0..<10:
-                    btn.reverbUnit.wetDryMix = 0
-                    
-                case 10..<20:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.smallRoom)
-                    break
-                case 20..<30:
-                    
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.smallRoom)
-                    break
-                    
-                case 30..<40:
-                    
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.mediumRoom)
-                    break
-                    
-                case 40..<50:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.largeRoom)
-                    break
-                    
-                case 50..<60:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.mediumHall)
-                    break
-                    
-                case 60..<70:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.largeHall)
-                    break
-                case 70..<80:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.mediumHall)
-                    break
-                case 80..<90:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.smallRoom)
-                    break
-                case 90..<100:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.plate)
-                    break
-                case 100..<110:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.mediumChamber)
-                    break
-                case 110..<120:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.largeChamber)
-                    break
-                    
-                case 120..<130:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.cathedral)
-                    break
-                case 130..<140:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.largeRoom2)
-                    break
-                case 140..<150:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.mediumHall2)
-                    break
-                case 150..<160:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.mediumHall3)
-                    break
-                case 160..<170:
-                    btn.reverbUnit.loadFactoryPreset(AVAudioUnitReverbPreset.largeHall2)
-                    break
-                 
-                }
- */
             }
- 
         }
     }
     
-
+    @IBAction func distorsionChanged(_ sender: UISlider) {
+        sender.isContinuous = false // stops continuous updating
+        
+        for case let btn as AudioButton in self.view.subviews {
+            if(btn.btnSelected){
+                btn.distorsionVal = sender.value
+            }
+        }
+        
+    }
+    
+    @IBAction func speedChanged(_ sender: UISlider) {
+        sender.isContinuous = false // stops continuous updating
+        
+        for case let btn as AudioButton in self.view.subviews {
+            if(btn.btnSelected){
+                btn.playBackVal = sender.value
+            }
+        }
+    }
+    
+    @IBAction func handlePlayButton(_ sender: ShapeButton) {
+        
+        UIView.animate(withDuration: 1.5, delay: 0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            
+            sender.layer.cornerRadius = 50
+            print("ANIMATING CHANGE")
+            /*
+             if let image = drawCustomImage(size:sender.frame.size,imageShape: sender.buttonShape,color:sender.shapeColor) {
+             sender.setImage(image, for: .normal)
+             }
+             */
+        })
+        view.layoutIfNeeded()
+    }
+    
+    
 }
 
